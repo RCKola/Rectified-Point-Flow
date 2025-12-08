7d8e9aa0df403b2332a6d325eedb606228b29c66
zhiachen <zhiachen@ethz.ch>
2025-12-08 11:45:55 +0100

    feat: extract_features.py with pc encoder

--- extract_features.py
+++ extract_features.py
@@ -1,29 +1,26 @@
 """Extract features from pointclouds and visualize PCA."""
 
-from functools import partial
-from typing import Dict
+from typing import Dict, Optional
 import numpy as np
 import torch
 import rectified_point_flow.encoder.concerto.concerto as concerto
 import rectified_point_flow.data as data
 
-try:
-    import flash_attn
-except ImportError:
-    flash_attn = None
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print("Using device:", device)
+import logging
+from pathlib import Path
+import os
+import warnings
 
-def get_pca_color(feat, brightness=1.25, center=True):
-    u, s, v = torch.pca_lowrank(feat, center=center, q=6, niter=5)
-    projection = feat @ v
-    projection = projection[:, :3] * 0.6 + projection[:, 3:6] * 0.4
-    min_val = projection.min(dim=-2, keepdim=True)[0]
-    max_val = projection.max(dim=-2, keepdim=True)[0]
-    div = torch.clamp(max_val - min_val, min=1e-6)
-    color = (projection - min_val) / div * brightness
-    color = color.clamp(0.0, 1.0)
-    return color
+import hydra
+import lightning as L
+from omegaconf import DictConfig
+
+from rectified_point_flow.utils import load_checkpoint_for_module, download_rfp_checkpoint, print_eval_table
+
+logger = logging.getLogger("ExtractFeatures")
+warnings.filterwarnings("ignore", module="lightning")
+warnings.filterwarnings("ignore", category=FutureWarning)
+
 
 def extract_point_features(encoder, batch: Dict[str, torch.Tensor]) -> tuple:
     """Extract point features using the encoder."""
@@ -68,16 +65,14 @@ def extract_point_features(encoder, batch: Dict[str, torch.Tensor]) -> tuple:
         features = point["feat"].reshape(B, N, -1)
     return features, point, n_valid_parts
 
-def visualize_features(features: torch.Tensor, coords: torch.Tensor, id: int = 0):
+def visualize_features(features: torch.Tensor, coords: torch.Tensor, save_dir: str, id: int = 0):
     """Visualize point features using PCA coloring."""
     pca_color = get_pca_color(features, brightness=1.2, center=True)
-    # batched_coord = point_data["coord"].clone()
-    # batched_coord[:, 0] += point_data["batch"] * 8.0
 
     from rectified_point_flow.visualizer import VisualizationCallback
-    vis_cb = VisualizationCallback(save_dir="outputs/concerto_features", image_size=2048)
+    vis_cb = VisualizationCallback(save_dir=save_dir, image_size=2048)
     vis_cb.setup(None, None, 'test')
-    print("save_dir", vis_cb.save_dir)
+
     vis_cb._save_sample_images(
         points = coords,
         colors = pca_color,
@@ -98,24 +93,17 @@ def run_visuals(load_path: str):
         visualize_features(
             features=point_features[i],
             coords=coords,
+            save_dir="outputs/concerto_features",
             id=i
         )
 
-
-def main():
-    import argparse
-    parser = argparse.ArgumentParser(description='Extract features from concerto.')
-    parser.add_argument('-d','--data-path', type=str, required=False, default=None,
-                        help='Path to the point cloud data file.')
-    parser.add_argument('-v','--visualize', action='store_true',
-                        help='Whether to visualize the extracted features.')
-    parser.add_argument('-l','--load-path', type=str, required=False, default=None,
-                        help='Path to load the encoded data.')
-    args = parser.parse_args()
-
-    if args.load_path is not None:
-        run_visuals(args.load_path)
-        return
+def load_concerto():
+    try:
+        import flash_attn
+    except ImportError:
+        flash_attn = None
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    logger.info("Using device:", device)
 
     concerto.utils.set_seed(42)
     if flash_attn is not None:
@@ -129,9 +117,80 @@ def main():
             "concerto_large", repo_id="Pointcept/Concerto", custom_config=custom_config
         ).to(device)
     
-    model.enc_mode = True
-    print("Model loaded.")
-    print("Model expected input dim", model.embedding.in_channels)
+    logger.info(f"Concerto loaded. Expecting input dim: {model.embedding.in_channels}")
+    return model
+
+
+def setup(cfg: DictConfig):
+    """Setup inference components."""
+    
+    ckpt_path = cfg.get("ckpt_path", None)
+    if ckpt_path is None:
+        ckpt_path = download_rfp_checkpoint("RPF_base_pretrain_ep600.ckpt", './weights')
+    elif not os.path.exists(ckpt_path):
+        logger.error(f"Checkpoint not found: {ckpt_path}")
+        logger.error("Please provide a valid checkpoint in the config or via ckpt_path='...' argument")
+        exit(1)
+
+    # Seed if set
+    seed = cfg.get("seed", None)
+    if seed is not None:
+        L.seed_everything(seed, workers=True, verbose=False)
+        logger.info(f"Seed set to {seed} for overlap prediction")
+
+    datamodule: L.LightningDataModule = hydra.utils.instantiate(cfg.data)
+    model = hydra.utils.instantiate(cfg.model)
+    load_checkpoint_for_module(model, ckpt_path)
+    model.eval()
+    
+    vis_config = cfg.get("visualizer", {})
+    callbacks = []
+    if vis_config and cfg["visualizer"]["renderer"] != "none":
+        vis_callback = hydra.utils.instantiate(vis_config)
+        callbacks.append(vis_callback)
+    
+    trainer: L.Trainer = hydra.utils.instantiate(
+        cfg.trainer,
+        callbacks=callbacks,
+        enable_checkpointing=False,
+        logger=False,
+    )
+    
+    return model, datamodule, trainer
+
+
+class ConcertoModule(L.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.model = load_concerto()
+    
+    def forward(self, batch: Dict[str, torch.Tensor], batch_idx: Optional[int] = None) -> Dict[str, torch.Tensor]:
+        """Forward pass of the model."""
+        point_features, point_data, n_valid_parts = extract_point_features(self.model.embedding, batch)
+        output = {
+            "point": point_data,
+            "n_valid_parts": n_valid_parts,
+        }
+        return output
+
+    def test_step(self, batch, batch_idx):
+        results = self.forward(batch)
+        return results
+
+
+@hydra.main(version_base="1.3", config_path="./config", config_name="RPF_REPA_extract_features")
+def main(cfg: DictConfig):
+    model, datamodule, trainer = setup(cfg)
+    eval_results = trainer.test(
+        model=model, 
+        datamodule=datamodule, 
+        verbose=False,
+    )
+    # print_eval_table(eval_results, datamodule.dataset_names)
+    logger.info("Visualizations saved to:" + str(Path(cfg.get('log_dir')) / "visualizations"))
+
+    # concerto_module = ConcertoModule()
+    return
     
     # Load data
     datamodule = data.PointCloudDataModule(
@@ -145,27 +204,34 @@ def main():
     loader = datamodule.train_dataloader()
     sample = next(iter(loader))
     print("Sample keys:", sample.keys())
-    print("Sample pointclouds shape:", sample["pointclouds"].shape)
     print("Sample num parts", sample["num_parts"])
     print("Sample loaded.")
     
     # Extract features
     point_features, point_data, _ = extract_point_features(model, sample)
-
     print("Feature extraction completed.")
-    # print("Extracted point features shape:", point_features.shape)
-    # print("Extracted features:", point_features)
-    # print("Point coordinates keys:", point_data.keys())
 
     # Save features and coordinates
+    filepath = save_dir + "/ikea_sample_features.npz"
     np.savez(
-        "outputs/ikea_sample_features.npz",
+        filepath,
         point_features=point_features.cpu().detach().numpy(),
         point_coords=point_data["coord"].cpu().detach().numpy(),
         index=sample["index"],
         name=sample["name"],
         dataset=sample["dataset_name"],
     )
+    print(f"Features saved to {filepath}")
+
+    # Visualize features
+    batch_coords = point_data["coord"].reshape(point_features.shape[0], -1, 3)
+    for i in range(point_features.shape[0]):
+        visualize_features(
+            features=point_features[i],
+            coords=batch_coords[i],
+            save_dir=save_dir,
+            id=i
+        )
 
 
 if __name__ == "__main__":
